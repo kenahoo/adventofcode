b49f5c753384e60d5452dbec73e4f85141c6d95f
Ken Williams <Ken.Williams@WindLogics.com>
2023-12-05 17:38:31 -0600

    Final solution, works

--- 2023/solutions/5b.py
+++ 2023/solutions/5b.py
@@ -1,7 +1,9 @@
+import logging
 import re
 
 
 class Ival:
+    """A single interval, with a target value that tells how to map values"""
     def __init__(self, low, high, target=None):
         self.low = low
         self.high = high
@@ -16,17 +18,18 @@ class Ival:
     def overlaps(self, other):
         return self.low <= other.high and other.low <= self.high
 
-    def map(self, seed):
-        return seed - self.low + self.target
+    def _map(self, num):
+        return num - self.low + self.target
 
     def map_ival(self, other):
-        if other.low in self and other.high in self:
-            return [Ival(self.map(other.low), self.map(other.high), None)]
-        if other.low < self.low:
-            return []
+        # 'other' must be contained in 'self'
+        if other.low < self.low or other.high > self.high:
+            raise ValueError(f"Can't map {other} to {self}")
+        return [Ival(self._map(other.low), self._map(other.high), None)]
 
 
 class Ivals:
+    """A collection of intervals that can map values"""
     def __init__(self):
         self.ivals: list[Ival] = []
 
@@ -38,36 +41,29 @@ class Ivals:
         self.ivals = sorted(self.ivals, key=lambda x: x.low)  # Keep 'em sorted
 
     def disjoint(self):
+        """Check whether all these intervals are disjoint - lucky for us, all the x-to-y maps are."""
         for i in range(len(self.ivals) - 1):
             if self.ivals[i].overlaps(self.ivals[i + 1]):
                 return False
         return True
 
-    def map_ival(self, other: Ival):
+    def map_ival(self, other: Ival) -> list[Ival]:
+        """Map 'other' through all the regions (intervals and areas outside intervals) it overlaps with"""
         for iv in self.ivals:
             if other.overlaps(iv):
                 if other.low < iv.low:
                     return [Ival(other.low, iv.low - 1),
                             *self.map_ival(Ival(iv.low, other.high))]
                 if other.high <= iv.high:
-                    return [iv.map(Ival(other.low, other.high))]
-                return [iv.map(Ival(other.low, iv.high)),
+                    return iv.map_ival(Ival(other.low, other.high))
+                return [*iv.map_ival(Ival(other.low, iv.high)),
                         *self.map_ival(Ival(iv.high + 1, other.high))]
 
         return [other]
 
 
-class IvalsStack:
-    def __init__(self, ivalses: list[Ivals] = None):
-        self.ivalses: ivalses or []
-
-    def map_ival(self, other: Ival):
-        for ivals in self.ivalses:
-            ivals.map_ival(other)
-
-
-# Construct all the mappings
 def get_mappings(data):
+    """Construct all the mappings"""
     maps = re.findall(r'\w+-to-\w+ map:((?:\s+\d+)+)', data)
     for i in range(len(maps)):
         ivals = Ivals()
@@ -80,24 +76,33 @@ def get_mappings(data):
     return maps
 
 
-# Find all the seed ranges
 def get_seeds(data):
+    """Find all the seed ranges"""
     m = re.search(r'seeds: [\d ]+', data)
     seed_str = m.group(0)
-    return [Ival(int(x[0]), int(x[1])) for x in re.findall(r'(\d+) (\d+)', seed_str)]
+    return [Ival(int(x[0]), int(x[1])+int(x[0])-1) for x in re.findall(r'(\d+) (\d+)', seed_str)]
 
 
-with open('data/5') as fh:
-    data = fh.read()
+def main():
+    logging.getLogger().setLevel(logging.WARNING)
 
-maps = get_mappings(data)
-seeds = get_seeds(data)
+    with open('data/5') as fh:
+        data = fh.read()
 
-lowest = 1e20
-while seeds:
-    seed = seeds.pop(0)
-    for map in maps:
-        result = map.map_ival(seed)
+    maps = get_mappings(data)
+    seeds = get_seeds(data)
 
-print(seeds)
+    for m in maps:
+        logging.info(f"Mapping:\n{m}")
+        mapped = []
+        for seed in seeds:
+            logging.info(f"Seed:{seed}")
+            result = m.map_ival(seed)
+            logging.info(f"Result:{result}")
+            mapped += result
+        seeds = mapped
 
+    print(min(seed.low for seed in seeds))
+
+
+main()
