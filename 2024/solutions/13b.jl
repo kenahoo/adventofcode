7b494b650b4fa585141b607d1b4f4e1408102023
Ken Williams <kenahoo@gmail.com>
2024-12-13 22:08:13 -0600

    Day 13

---
+++ 2024/solutions/13b.jl
@@ -0,0 +1,25 @@
+using LinearAlgebra
+
+function load(file)
+    map(assemble, eachmatch(r"A: X.(\d+), Y.(\d+).*?B: X.(\d+), Y.(\d+).*?Prize: X=(\d+), Y=(\d+)"s, read(file, String)))
+end
+
+function assemble(m::RegexMatch)
+    c = parse.(BigInt, m.captures)
+    [c[1] c[3]; c[2] c[4]], [c[5], c[6]] .+ BigInt(10000000000000)
+end
+
+function solve(M, b)
+    try
+        return Int.(round.(M \ b, digits=15)) ⋅ [3, 1]
+    catch e
+        e isa InexactError && return 0
+        rethrow(e)
+    end
+end
+
+##################################
+
+d = load("data/13.txt")
+
+println(sum(solve(x...) for x in d))
